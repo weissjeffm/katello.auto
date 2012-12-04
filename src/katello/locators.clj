fc4f62233f8539d97ecb8fc01c1932d7aa811ddd
Jeff Weiss <jweiss@redhat.com>
2012-12-04 11:56:05 -0500

    Remove unused locator

--- src/katello/locators.clj
+++ src/katello/locators.clj
@@ -220,7 +220,6 @@
    :product-description-text            "//*[@name='product[description]']"
    :remove-product                      (link "Remove Product")
    ;;add repo
-   :add-repository                      "//ul[//div[starts-with(@id,'edit_product') and normalize-space(.)='$1']]//div[starts-with(@id,'add_repository')]"
    :repo-name-text                      "repo[name]"
    :repo-label-text                     "repo[label]"
    :repo-url-text                       "repo[feed]" 
